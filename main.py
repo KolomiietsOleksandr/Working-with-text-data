f19ed3084d25d78cb5fc937a6db1cce9882218db
shito.bulo@gmail.com <shito.bulo@gmail.com>
2022-12-12 22:37:50 +0200

    Finished task2 and removed iterator i output from task1

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def medals(filename,country,year, output):
                             #Add result to list
                             res = res + res_list.join(list)
                             i += 1
-                            print(i)
+                            
                             #Print result
                             if i == 10:
                                 print(res)
@@ -48,6 +48,41 @@ def medals(filename,country,year, output):
                     file.write(res)
                     file.write(total_medals)
 
+def total(filename,year):
+
+    
+    with open(filename, "r") as file:
+            while True:
+                line = file.readline()
+                if not line: break
+
+                #Split data by information
+                info = line.split('\t')
+                _team = info[6]
+                _noc = info[7]
+                _year = info[9]
+                _medal = info[14]
+                if year == _year:
+                    if _team not in C_M:
+                        C_M[_team]= [0, 0, 0]
+                    if _medal != 'NA\n':
+                                #Count total medals
+                                if _medal == 'Gold\n': C_M[_team][0] += 1
+                                if _medal == 'Silver\n': C_M[_team][1] += 1
+                                if _medal == 'Bronze\n': C_M[_team][2] += 1
+    for country in C_M:
+        total=C_M[country][0] + C_M[country][1] + C_M[country][2]
+        if total < 1:
+            continue
+        else:
+            print(f"{country} has total {total} medals: {C_M[country][0]} Gold, {C_M[country][1]} Silver, {C_M[country][2]}")
+                    
+C_M = {
+
+    }
+                
+
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -55,14 +90,15 @@ def main():
     parser.add_argument("--command", required=True)
     #Add arguments
     parser.add_argument("--filename", required=True)
-    parser.add_argument("--country", required=True)
-    parser.add_argument("--year", required=True)
+    parser.add_argument("--country", required=False)
+    parser.add_argument("--year", required=False)
     parser.add_argument("--output",type=bool, required=False)
 
     args = parser.parse_args()
 
     if args.command == "medals":
         medals(args.filename,args.country,args.year,args.output)
-
+    if args.command == "total":
+        total(args.filename,args.year)
 
 main()
