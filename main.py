53b15d0610f76df81f79b73b8be17ca0b23c9998
Oleksandr Kolomiiets <alexcrimson0818@gmail.com>
2022-12-14 00:59:28 +0200

    add -overall command

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import argparse
 
-#First task
-def medals(filename,country,year, output):
+
+# First task
+def medals(filename, country, year, output):
     gold = 0
     silver = 0
     bronze = 0
@@ -12,94 +13,130 @@ def medals(filename,country,year, output):
     i = 0
 
     with open(filename, "r") as file:
-            while True:
-                line = file.readline()
-                if not line: break
+        while True:
+            line = file.readline()
+            if not line: break
 
-                #Split data by information
-                info = line.split('\t')
-                _name = info[1]
-                _team = info[6]
-                _noc = info[7]
-                _year = info[9]
-                _sport = info[12]
-                _medal = info[14]
-                if year == _year:
-                    if country == _noc or country == _team:
-                        if _medal != 'NA\n':
-                            list = (f'{_name}-{_sport}-{_medal}')
-                            #Count total medals
-                            if _medal == 'Gold\n': gold += 1
-                            if _medal == 'Silver\n': silver += 1
-                            if _medal == 'Bronze\n': bronze += 1
-
-                            total_medals = (f'Total medals: Gold - {gold}, Silver - {silver}, Bronze - {bronze}')
-                            #Add result to list
-                            res = res + res_list.join(list)
-                            i += 1
-                            
-                            #Print result
-                            if i == 10:
-                                print(res)
-                                print(total_medals)
-                                break
-            if output == True:
-                with open('result.txt','w') as file:
-                    file.write(res)
-                    file.write(total_medals)
-
-def total(filename,year):
-
-    
-    with open(filename, "r") as file:
-            while True:
-                line = file.readline()
-                if not line: break
-
-                #Split data by information
-                info = line.split('\t')
-                _team = info[6]
-                _noc = info[7]
-                _year = info[9]
-                _medal = info[14]
-                if year == _year:
-                    #check if country already exists in dict
-                    if _team not in C_M:
-                        C_M[_team]= [0, 0, 0]
+            # Split data by information
+            info = line.split('\t')
+            _name = info[1]
+            _team = info[6]
+            _noc = info[7]
+            _year = info[9]
+            _sport = info[12]
+            _medal = info[14]
+            if year not in file: print('Invalid year')
+            elif country not in file: print('Invalid country')
+            if year == _year:
+                if country == _noc or country == _team:
                     if _medal != 'NA\n':
-                                #Count total medals
-                                if _medal == 'Gold\n': C_M[_team][0] += 1
-                                if _medal == 'Silver\n': C_M[_team][1] += 1
-                                if _medal == 'Bronze\n': C_M[_team][2] += 1
+                        list = (f'{_name}-{_sport}-{_medal}')
+                        # Count total medals
+                        if _medal == 'Gold\n': gold += 1
+                        if _medal == 'Silver\n': silver += 1
+                        if _medal == 'Bronze\n': bronze += 1
+
+                        total_medals = (f'Total medals: Gold - {gold}, Silver - {silver}, Bronze - {bronze}')
+                        # Add result to list
+                        res = res + res_list.join(list)
+                        i += 1
+
+                        # Print result
+                        if i == 10:
+                            print(res)
+                            print(total_medals)
+                            break
+        if output == True:
+            with open('result.txt', 'w') as file:
+                file.write(res)
+                file.write(total_medals)
+
+
+# Second task
+def total(filename, year):
+    with open(filename, "r") as file:
+        # Creating a dictionary that stores medals for each country
+        C_M = {}
+        while True:
+            line = file.readline()
+            if not line: break
+
+            # Split data by information
+            info = line.split('\t')
+            _team = info[6]
+            _noc = info[7]
+            _year = info[9]
+            _medal = info[14]
+            if year == _year:
+                # check if country already exists in dict
+                if _team not in C_M:
+                    C_M[_team] = [0, 0, 0]
+                if _medal != 'NA\n':
+                    # Count total medals
+                    if _medal == 'Gold\n': C_M[_team][0] += 1
+                    if _medal == 'Silver\n': C_M[_team][1] += 1
+                    if _medal == 'Bronze\n': C_M[_team][2] += 1
     for country in C_M:
-        total=C_M[country][0] + C_M[country][1] + C_M[country][2]
+        total = C_M[country][0] + C_M[country][1] + C_M[country][2]
         if total < 1:
             continue
         else:
             print(f"{country} has total {total} medals: {C_M[country][0]} Gold, {C_M[country][1]} Silver, {C_M[country][2]}")
-#Creating a dictionary that stores medals for each country                    
-C_M = {
 
-    }
-                
 
+# Third task
+def overall(filename, countries):
+    with open(filename, 'r') as file:
+        medals_by_year = {}
+
+        for country in countries:
+            while True:
+                line = file.readline()
+                if not line:
+                    break
+
+                # Split data by information
+                info = line.strip().split('\t')
+                _team = info[6]
+                _noc = info[7]
+                _year = info[9]
+                _medal = info[14]
+
+                if _team == country:
+                    if _medal != "NA":
+                        #Add 1 medal to year if we have key
+                        if _year in medals_by_year:
+                            medals_by_year[_year] = medals_by_year[_year] + 1
+                        #If we don`t have key - create it and add 1 medal
+                        else:
+                            medals_by_year[_year] = 0
+            #Find in each year was maximum of medals
+            year_result = max(medals_by_year, key=medals_by_year.get)
+            print(f'{country} has record of medals in {year_result}: {medals_by_year[year_result]} medals')
+            medals_by_year.clear()
+            #Reset while
+            file.seek(0)
 
 
 def main():
     parser = argparse.ArgumentParser()
-    #Add command
-    parser.add_argument("--command", required=True)
-    #Add arguments
-    parser.add_argument("--filename", required=True)
+    # Add command
+    parser.add_argument("--command", required=False)
+    parser.add_argument('--overall', nargs='+', required=False)
+    # Add arguments
+    parser.add_argument("--filename", required=False)
     parser.add_argument("--country", required=False)
     parser.add_argument("--year", required=False)
-    parser.add_argument("--output",type=bool, required=False)
+    parser.add_argument("--output", type=bool, required=False)
 
     args = parser.parse_args()
 
     if args.command == "medals":
-        medals(args.filename,args.country,args.year,args.output)
+        medals(args.filename, args.country, args.year, args.output)
     if args.command == "total":
-        total(args.filename,args.year)
+        total(args.filename, args.year)
+    if args.overall:
+        overall(args.filename, args.overall)
+
 
 main()
